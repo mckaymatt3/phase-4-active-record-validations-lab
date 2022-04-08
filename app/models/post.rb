f96df0579ef3a4b337f86dd7471ee7b3041a16d7
mckaymatt3 <mckaymatt@outlook.com>
2022-04-08 10:53:00 -0400

    Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, presence: true, length: {minimum: 250} 
+    validates :summary,  presence: true, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+        # message: "%{value} is not a valid category" }
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+    
+      def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
+
 end
